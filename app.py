fd0d6efc62b85b35a4163fd3ef5751c51cd170f3
Anirudh Gupta <anirudhgupta421@gmail.com>
2024-11-16 11:03:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,9 +31,9 @@ if st.button("Predict Fraud"):
 
     # Prerequisite Condition
     if transaction_amount < old_balance:
-        print(f"Transaction approved! Remaining balance: {old_balance - transaction_amount}")
+        st.success(f"Transaction approved! Remaining balance: {old_balance - transaction_amount}")
     else:
-        print("Error: Transaction amount must be strictly less than the old balance.")
+        st.error("Error: Transaction amount must be strictly less than the old balance.")
     
     # Display result
     if prediction[0] == 'Fraud':
